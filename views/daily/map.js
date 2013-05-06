9c1f358757cb0bc8b3b641d492719f498addee23
Charles Lehner <celehner1@gmail.com>
2013-05-05 22:36:30 -0400

    Make view functions erica-compatible.
    reduce cannot seem to have functions outside the main function

--- views/daily/map.js
+++ views/daily/map.js
@@ -1,18 +1,18 @@
-function mapMessageAmounts(msg) {
-	if (typeof msg.text != "string") return;
-	var d = new Date((msg.date || msg._id * 1000));
-	if (isNaN(d)) return;
-	// shift midnight to 6AM
-	d.setHours(d.getHours()-6);
-	var Y = d.getFullYear(),
-	M = d.getMonth() + 1,
-	D = d.getDate();
-
-	//emit([channel, Y, M, D, msg.sender], null);
-	emit([Y, M, D, msg.sender], null);
-}
-
 function(doc) {
+	function mapMessageAmounts(msg) {
+		if (typeof msg.text != "string") return;
+		var d = new Date((msg.date || msg._id * 1000));
+		if (isNaN(d)) return;
+		// shift midnight to 6AM
+		d.setHours(d.getHours()-6);
+		var Y = d.getFullYear(),
+		M = d.getMonth() + 1,
+		D = d.getDate();
+
+		//emit([channel, Y, M, D, msg.sender], null);
+		emit([Y, M, D, msg.sender], null);
+	}
+
 	if (doc._id.indexOf("-") != -1) {
 		// If this doc just has a start and end time for a bunch of messages,
 		// emit the messages at regular intervals between the start and end
--- views/hourly_distribution/map.js
+++ views/hourly_distribution/map.js
@@ -1,21 +1,21 @@
-function mapMessageHour(msg) {
-	if (typeof msg.text != "string") return;
-	var d = new Date(msg.date || (msg._id * 1000));
-	if (isNaN(d)) return;
-	// shift midnight to 6AM
-	d.setHours(d.getHours()-6);
-	var Y = d.getFullYear(),
-	M = d.getMonth() + 1,
-	D = d.getDate(),
-	h = d.getHours();
-	var hours = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-	hours[h] = 1;
-
-	//emit([msg.channel, Y, M, D, msg.sender], hours);
-	emit([Y, M, D, msg.sender], hours);
-}
-
 function(doc) {
+	function mapMessageHour(msg) {
+		if (typeof msg.text != "string") return;
+		var d = new Date(msg.date || (msg._id * 1000));
+		if (isNaN(d)) return;
+		// shift midnight to 6AM
+		d.setHours(d.getHours()-6);
+		var Y = d.getFullYear(),
+		M = d.getMonth() + 1,
+		D = d.getDate(),
+		h = d.getHours();
+		var hours = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+		hours[h] = 1;
+
+		//emit([msg.channel, Y, M, D, msg.sender], hours);
+		emit([Y, M, D, msg.sender], hours);
+	}
+
 	mapMessageHour(doc);
 	if (doc.messages) doc.messages.forEach(mapMessageHour);
 }
